f044098c66b3719ea9148c209c77e4942e1df26d
Stanislav Kopiievskyi <47134949+lIlDirE@users.noreply.github.com>
2023-05-09 20:16:08 +0300

    Update apiUsers.js

--- src/service/apiUsers.js
+++ src/service/apiUsers.js
@@ -4,13 +4,6 @@ axios.defaults.baseURL = "https://645a419e95624ceb21fc636c.mockapi.io/";
 
 export const getAllPageTweets = async (filter) => {
    let url = `/users`;
-   if (filter === "Follow") {
-      url = `/users?follow=false`;
-   } else if (filter === "Following") {
-      url = `/users?follow=true`;
-   } else {
-      url = `/users`;
-   }
 
    try {
       const { data } = await axios.get(url);
